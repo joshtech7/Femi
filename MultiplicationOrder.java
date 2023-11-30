d26e9c25650db9706bbe7948222a2937b4e1467c
joshtech7 <151773650+joshtech7@users.noreply.github.com>
2023-11-30 17:28:54 +0100

    Add files via upload

---
+++ MultiplicationOrder.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MultiplicationOrder{
+	
+
+		public static void main(String[] balikis){
+		Scanner abbeyLight = new Scanner(System.in);
+		System.out.print("ozuwo enter number: ");
+		int number = abbeyLight.nextInt();
+
+		int count = 1;
+		
+
+		while(count <= 12){
+	
+		
+		    System.out.printf("%d * %d = %%n", number, count, count * number);
+		count ++;
+		} 
+	}
+}	
